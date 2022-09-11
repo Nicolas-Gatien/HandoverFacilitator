f6794af0441ab70439814140c946ce34b91491ed
polarcube1 <polarcube1@gmail.com>
2022-09-11 16:42:18 -0400

    🎨 HTML Injection

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -1,3 +1,16 @@
+import Dashboard from "./views/Dashboard.js";
+
+const pathToRegex = path => new RegExp("^" + path.replace(/\//g, "\\/").replace(/:\w+/g, "(.+)") + "$");
+
+const getParams = match => {
+    const values = match.result.slice(1);
+    const keys = Array.from(match.route.path.matchAll(/:(\w+)/g)).map(result => result[1]);
+
+    return Object.fromEntries(keys.map((key, i) => {
+        return [key, values[i]];
+    }));
+};
+
 const navigateTo = url => {
     history.pushState(null, null, url);
     router();
@@ -5,41 +18,40 @@ const navigateTo = url => {
 
 const router = async () => {
     const routes = [
-        { path: "/", view: () => console.log("Viewing Dashboard") },
-        { path: "/posts", view: () => console.log("Viewing Posts") },
-        { path: "/settings", view: () => console.log("Viewing Settings") },
+        { path: "/", view: Dashboard },
     ];
 
     // Test each route for potential match
     const potentialMatches = routes.map(route => {
-        return{
+        return {
             route: route,
-            isMatch: location.pathname === route.path
+            result: location.pathname.match(pathToRegex(route.path))
         };
     });
 
-    let match = potentialMatches.find(potentialMatch => potentialMatch.isMatch);
+    let match = potentialMatches.find(potentialMatch => potentialMatch.result !== null);
 
-    if (!match){
-        math = {
+    if (!match) {
+        match = {
             route: routes[0],
-            isMatch: true
+            result: [location.pathname]
         };
     }
 
-    console.log(match.route.view());
+    const view = new match.route.view(getParams(match));
+
+    document.querySelector("#app").innerHTML = await view.getHtml();
 };
 
 window.addEventListener("popstate", router);
 
 document.addEventListener("DOMContentLoaded", () => {
     document.body.addEventListener("click", e => {
-        if (e.target.matches("[data-link]")){
+        if (e.target.matches("[data-link]")) {
             e.preventDefault();
             navigateTo(e.target.href);
         }
     });
-
 
     router();
 });
