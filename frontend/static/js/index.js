ada92f9c24e4f56d28bc15f6390cc0bd640a5fd3
polarcube1 <polarcube1@gmail.com>
2022-09-11 17:10:40 -0400

    📃 Added Individual Pages

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -1,5 +1,6 @@
 import Dashboard from "./views/Dashboard.js";
 import Posts from "./views/Posts.js";
+import PostView from "./views/PostView.js";
 import Settings from "./views/Settings.js";
 
 const pathToRegex = path => new RegExp("^" + path.replace(/\//g, "\\/").replace(/:\w+/g, "(.+)") + "$");
@@ -22,7 +23,8 @@ const router = async () => {
     const routes = [
         { path: "/", view: Dashboard },
         { path: "/posts", view: Posts },
-        { path: "/settings", view: Settings },
+        { path: "/posts/:id", view: PostView },
+        { path: "/settings", view: Settings }
     ];
 
     // Test each route for potential match
